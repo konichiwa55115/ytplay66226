3ae9cca6e9b3c9ac5a52636dd35e1cbbabf41a0f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-15 21:21:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,61 +1,31 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import ForceReply
-import subprocess
 import shutil
 from os import system as cmd
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
 
-CHOOSE_UR_LANG = " Choose Your folmula ! "
-CHOOSE_UR_LANG_BUTTONS = [
-    [InlineKeyboardButton("vid 360p",callback_data="vid 360p")],
-     [InlineKeyboardButton("vid 720p",callback_data="vid 720p")],
-     [InlineKeyboardButton("aud",callback_data="aud")]
-]
-
-
 bot = Client(
-    "myfirs",
+    "playbot",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6030811502:AAF0tj9q_2BH1HpmRZLkuvBQttmxYdYFw6o"
+    bot_token="5545470781:AAGjzXBuTOeJ1JulON1Cw0I2ZP8kebs2Alk"
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " االسلام عليكم أنا بوت التحميل من يوتيوب  ",disable_web_page_preview=True)
+    bot.send_message(message.chat.id, " السلام عليكم أن بوت تحويل القنوات والقوائم إلى روابط\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
 
 @bot.on_message(filters.private & filters.incoming & filters.text  )
 def _telegram_file(client, message):
 
   
   user_id = message.from_user.id 
-  global url
-  url = message.text  
-  cmd(f'mkdir downloads')
-  message.reply(
-             text = CHOOSE_UR_LANG,
-             reply_markup = InlineKeyboardMarkup(CHOOSE_UR_LANG_BUTTONS)
-
-        )
-
-@bot.on_callback_query()
-def callback_query(CLIENT,CallbackQuery):
-  CallbackQuery.edit_message_text(
-      
-      "جار التنزيل "
-  )   
-  if CallbackQuery.data == "vid 360p":
-      cmd(f'yt-dlp -f 18 -o downloads/+"%(title)s.%(ext)s" {url}')
-  elif CallbackQuery.data == "vid 720p":
-      cmd(f'yt-dlp -f 22 -o downloads/+"%(title)s.%(ext)s" {url}')
-  elif CallbackQuery.data == "aud":
-      cmd(f'yt-dlp --extract-audio --audio-format mp3  -o downloads/+"%(title)s.%(ext)s" {url}')
-  cmd(f'uploadgram -1001821573758 ./downloads/')
-  shutil.rmtree('./downloads/')
-  CallbackQuery.edit_message_text(
-      
-      "تم التنزيل ✅"
-  )   
-
-
+  url = message.text
+  
+  message.reply_text("جار التحويل")
+  cmd(f'yt-dlp --flat-playlist -i --print-to-file url file.txt {url}')
+  with open('file.txt', 'r') as file:
+     info = file.read().rstrip('\n')   
+     message.reply_text(f'{info}', quote=True)
+  cmd(f'unlink file.txt')
 bot.run()
